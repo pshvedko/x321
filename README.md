48b46eb5fee768a3dd1f77d1971f89657f8a72e3
Pavel Shvedko <pshvedko@gmail.com>
2022-07-13 00:45:04 +0300

    +1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # x321
-temporary
+
+go run main.go http://127.0.0.1:8086/files/
+
+ffmpeg -i http://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3 -c copy -f rtsp rtsp://127.0.0.1:8554/123
--- main.go
+++ main.go
@@ -195,6 +195,7 @@ func (h *handler) Wait() {
 
 const FORMAT = "flv" // "matroska"
 
+// go run main.go http://127.0.0.1:8086/files/
 // ffmpeg -i http://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3 -c copy -f rtsp rtsp://127.0.0.1:8554/123
 func main() {
 	var graceFullStop bool
